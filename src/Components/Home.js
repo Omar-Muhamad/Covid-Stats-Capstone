0e16b391e80e3b0a2fdbd0e561d6d15fc26176ab
Omar-Muhamad <eng.omarmuhammad@gmail.com>
2022-02-02 20:24:06 +0200

    Add search bar

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -21,6 +21,12 @@ const Home = () => {
       setContries(Object.values(allCountries.dates)[0].countries);
     }
   }, [allCountries]);
+
+  const changeHandler = (e) => {
+    const value = e.target.value; // eslint-disable-line
+    console.log(value);
+  };
+
   return (
     <>
       {loading === 'loading' && <Loading />}
@@ -30,25 +36,39 @@ const Home = () => {
         <div className="globalStats">
           <h2 className="text-4xl font-bold text-center">Global Cases</h2>
           <p className="text-lg text-center">
-            {loading === 'success' && `${allCountries.total.today_confirmed} Cases`}
+            {loading === 'success' && (
+              `${allCountries.total.today_confirmed} Cases`
+            )}
           </p>
         </div>
       </section>
       <section className="countriesStats">
-        <h2 className="bg-[#35548B] py-[4px] px-4 text-white font-medium">
-          Stats by country
-        </h2>
-        <ul className="grid grid-cols-2 justify-items-end text-right bg-[#4972be]">
-          {loading === 'success' && Object.entries(countries).map(([key, value]) => (
-            <li key={key} className="country w-full p-4 text-white">
-              <Link to={value.name} className="w-full grid">
-                <i className="far fa-arrow-alt-circle-right fa-lg" />
-                <i className="fas fa-map-marker-alt fa-5x text-[#2D4573] justify-self-center" />
-                <h2 className=" text-xl font-bold">{value.name}</h2>
-                <p>{`${value.today_confirmed} Cases`}</p>
-              </Link>
-            </li>
-          ))}
+        <div className="flex justify-between items-center bg-[#35548B] py-[10px] px-4 text-white font-medium">
+          <h2>Stats by country</h2>
+          <div className="relative">
+            <input
+              type="text"
+              onChange={changeHandler}
+              name="searchBar"
+              id="searchBar"
+              placeholder="Search"
+              className="rounded-2xl px-4 py-1 bg-[#5787E5] focus:outline-none placeholder:text-white text-lg"
+            />
+            <i className="fas fa-search absolute right-[10px] top-2" />
+          </div>
+        </div>
+        <ul className="grid grid-cols-2 justify-items-end text-right bg-[#5787E5]">
+          {loading === 'success' && (
+            Object.entries(countries).map(([key, value]) => (
+              <li key={key} className="country w-full p-4 text-white">
+                <Link to={value.name} className="w-full grid">
+                  <i className="far fa-arrow-alt-circle-right fa-lg" />
+                  <i className="fas fa-map-marker-alt fa-5x text-[#2D4573] justify-self-center" />
+                  <h2 className=" text-xl font-bold">{value.name}</h2>
+                  <p>{`${value.today_confirmed} Cases`}</p>
+                </Link>
+              </li>
+            )))}
         </ul>
       </section>
     </>
